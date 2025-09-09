155ec95e887e361bc860e5eb21c3b17c188ef3c6
AzureAD\GautamBedi <gbedi@miningskills.com.au>
2025-09-09 16:27:40 +1000

    => Finished the background

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -1,11 +1,12 @@
 "use client";
-
 import HeroText from "../components/heroText";
+import ParalaxBackground from "../components/paralaxBackground";
 
 function Hero() {
   return (
     <section className="flex items-start justify-center md:justify-start min-h-screen overflow-hidden sm:px-10 px-5 lg:px-15">
       <HeroText />
+      <ParalaxBackground />
     </section>
   );
 }
