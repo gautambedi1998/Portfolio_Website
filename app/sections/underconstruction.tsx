e042fd964670e45547891a7b2071826541b45ded
AzureAD\GautamBedi <gbedi@miningskills.com.au>
2025-09-17 15:04:52 +1000

    => Fixed the margin

--- app/sections/underconstruction.tsx
+++ app/sections/underconstruction.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UnderConstruction = () => {
   return (
-    <section className="sm:px-10 px-5 lg:px-15  mt-20 md:mt-30">
+    <section className="sm:px-10 px-5 lg:px-15  mt-20 md:mt-10">
       <div className="flex flex-col  p-6 bg-gradient-to-b from-storm to-indigo rounded-2xl row-span-2 md:col-span-3 h-[10rem] md:h-full relative overflow-hidden hover:-translate-y-1 duration-200">
         <p className="mt-2 mb-2 text-xl">Projects and Work Experience</p>
         <p className="mt-2 mb-2 text-xl">
