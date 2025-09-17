dcb6498a3103529c38ec1567957d4725c0b2011f
AzureAD\GautamBedi <gbedi@miningskills.com.au>
2025-09-17 14:56:39 +1000

    => added the underconstruciton banner for the moment

---
+++ app/sections/underconstruction.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UnderConstruction = () => {
+  return (
+    <section className="sm:px-10 px-5 lg:px-15  mt-20 md:mt-30">
+      <div className="flex flex-col  p-6 bg-gradient-to-b from-storm to-indigo rounded-2xl row-span-2 md:col-span-3 h-[10rem] md:h-full relative overflow-hidden hover:-translate-y-1 duration-200">
+        <p className="mt-2 mb-2 text-xl">
+          Projects and Work Experience Section
+        </p>
+        <p className="mt-2 mb-2 text-xl">{"Under Construction :) "} </p>
+      </div>
+    </section>
+  );
+};
+
+export default UnderConstruction;
