b3a78c102f65e30d678190ba529af45361b653c2
AzureAD\GautamBedi <gbedi@miningskills.com.au>
2025-09-19 11:45:59 +1000

    => Added small change in the footer section

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -1,8 +1,8 @@
 import { mySocials } from "../constants/index";
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
-      <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 sm:px-10 px-5 lg:px-15 mt-16">
+      {/* <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" /> */}
       <div className="flex gap-2">
         <p>Terms & Conditions</p>
         <p>|</p>
